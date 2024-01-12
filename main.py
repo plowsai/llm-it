82fe75b7138cf67b95d6bed8f35b80804fa4802d
Jordan Plows <jordan@lightapi.com>
2024-01-11 18:43:47 -0800

    dev: first commit for the landing page

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask,  render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home ():
+    if request.method == 'POST':
+        user_input = request.form.get('text_field')
+        print(user_input)
+    return render_template('home.html')

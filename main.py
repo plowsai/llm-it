8a1c223c519a4c8b958ebacf9520e62e260ee306
Jordan Plows <jordan@lightapi.com>
2024-01-16 20:22:49 -0800

    dev: idk what this commit is

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import openai 
 import json 
 import os 
-import request
+# import request
 from flask import Flask,  render_template, request
 
 app = Flask(__name__)
@@ -10,5 +10,11 @@ app = Flask(__name__)
 def home ():
     if request.method == 'POST':
         user_input = request.form.get('text_field')
-        print(user_input)
+        openai_response = openai.Completion.create(
+          engine="text-davinci-003",
+          prompt=user_input,
+          max_tokens=150
+        )
+        print(openai_response.choices[0].text.strip())
     return render_template('home.html')
+
